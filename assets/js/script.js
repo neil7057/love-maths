60df0bd7c35a20bba1c5a1c46e4108e50ad2dfbf
Neil Allen <neil7057@gmail.com>
2024-07-05 00:43:19 +0000

    add favicon and final code tidy

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,6 +15,12 @@ document.addEventListener("DOMContentLoaded", function() {
         })
     }
 
+    document.getElementById("answer-box").addEventListener("keydown", function(event)  {
+        if (event.key ==="Enter") {
+            checkAnswer();
+        }
+    });
+
     runGame("addition");
 })
 
@@ -23,6 +29,10 @@ document.addEventListener("DOMContentLoaded", function() {
  * and after the user's answer has been processed
  */
 function runGame(gameType) {
+
+    document.getElementById("answer-box").value = "";
+    document.getElementById("answer-box").focus();
+
     let num1 = Math.floor(Math.random()*25) +1;
     let num2 = Math.floor(Math.random()*25) +1;
 
@@ -109,8 +119,8 @@ function displayAdditionQuestion(operand1, operand2) {
 }
 
 function displaySubtractQuestion(operand1, operand2) {
-    document.getElementById('operand1').textContent = operand1;
-    document.getElementById('operand2').textContent = operand2;
+    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
     document.getElementById('operator').textContent = "-";
 }
 
